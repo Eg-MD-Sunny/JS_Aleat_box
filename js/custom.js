37cace3cda2e84730f8156ca85f897a415ae1e00
Md Naimur Zaman Sunny <egmdsunny@gmail.com>
2022-10-01 23:19:40 +0600

    JS Aleat Box

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+//This is not Product Value
+console.log('This is First Comment.This is Second Comment.This is Third Comment');
+
+
+//*****| Alert Option Active
+//------------------------------------------
+function myFunction(){ 
+	alert("Submit Successfully");
+}
